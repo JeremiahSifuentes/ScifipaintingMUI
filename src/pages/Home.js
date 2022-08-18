40373e40547f54df71a3fbc16a9f665ac48bcda0
Jeremiah Sifuentes <jerrysifuentes01@gmail.com>
2022-08-18 14:21:21 -0500

    Setting up landing page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,33 +1,43 @@
 import React from "react";
-import '../App.css';
-import Container from '@mui/material/Container';
-import Grid from '@mui/material/Grid';
-import { Typography } from '@mui/material';
+import "../App.css";
+import Box from '@mui/material/Box';
+import { Grid } from '@mui/material';
+
 
 const Home = () => {
+
+  const imageUrl = window.innerWidth >= 650 ? '/landingImg.png' : '/landingImgMbl.png';
+
   return (
     <div>
       <div className="App">
-        <Container
-          sx={{
-            marginY: 5,
-          }}
-        >
-          <>
-           
-              <Typography
-                variant="h4"
-                component="h2"
-                marginTop={5}
-                marginBottom={3}
-              >
-                Test 
-              </Typography>
-          </>
-        </Container>
+        <Grid container>
+          <Grid item>Image and Mission statement</Grid>
+          <Grid item>Gallery</Grid>
+          <Grid item>Services</Grid>
+          <Grid item>Contact</Grid>
+        </Grid>
+        <Box>
+        </Box>
       </div>
     </div>
   );
 };
 
 export default Home;
+
+
+{/* <span>
+<div
+    style={{
+      position: 'fixed',
+      height: '100%',
+      width: '100%',
+      backgroundSize: 'cover',
+      backgroundImage: `url(${imageUrl})`,
+      backgroundRepeat: 'no-repeat'
+    }}
+  >
+  
+  </div>
+</span> */}
